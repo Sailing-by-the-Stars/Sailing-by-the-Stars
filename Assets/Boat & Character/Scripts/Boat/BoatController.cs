8953bf57827bbffd5992e920ecc0b36ff2ad3963
Johan Beimers <35890334+JohanOnPc@users.noreply.github.com>
2026-03-10 15:27:09 +0100

    Added functions that calculate the lift and drag forces generated by the sail

--- Assets/Boat & Character/Scripts/Boat/BoatController.cs
+++ Assets/Boat & Character/Scripts/Boat/BoatController.cs
@@ -4,12 +4,17 @@
 */
 
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BoatController : MonoBehaviour
 {
     [Header("Physics")]
     [SerializeField] private Vector3 wind = Vector3.zero;
     [SerializeField] private Vector3 apparentWind = Vector3.zero;
+    [SerializeField] private float dragCoefficient = 1.5f;
+    [SerializeField] private float liftCoefficient = 1.2f;
+    [SerializeField] private float sailArea = 20;
+    [SerializeField] private float airDensity = 1.225f;
 
     [Header("Boat stats")]
     [SerializeField] private float maxRudderDeflection = 20f;
@@ -21,6 +26,7 @@ public class BoatController : MonoBehaviour
     [Header("")]
     [SerializeField] private GameObject rudderObject;
     [SerializeField] private GameObject telltaleObject;
+    [SerializeField] private GameObject mastObject;
 
 
     private Rigidbody rigidBody;
@@ -40,8 +46,35 @@ public class BoatController : MonoBehaviour
     void FixedUpdate()
     {
         apparentWind = wind - rigidBody.linearVelocity;
+        Vector3 mastDirection = Vector3.ProjectOnPlane(mastObject.transform.right, Vector3.up).normalized;
+        Vector3 windDirection = Vector3.ProjectOnPlane(apparentWind, Vector3.up).normalized;
+
+        float mastDirectionIntoWind = Vector3.SignedAngle(-mastDirection, windDirection, Vector3.up);
+
+        Debug.Log("Angle of Attack: " + mastDirectionIntoWind);
         //rigidBody.AddRelativeForce(new Vector3(40f ,0f, 0f), ForceMode.Force);
         //rigidBody.AddRelativeTorque(new Vector3(0f, 10f, 0f), ForceMode.Force);
+
+        float drag = CalculateDrag(apparentWind.magnitude, mastDirectionIntoWind);
+        float lift = CalculateLift(apparentWind.magnitude, mastDirectionIntoWind);
+    }
+
+    //Calculates the force of drag experienced on the sail, used when running downwind and broadreach
+    float CalculateDrag(float apparentWindSpeed, float apparentWindAngle)
+    {
+        float drag = .5f * airDensity * apparentWindSpeed * apparentWindSpeed * sailArea * dragCoefficient;
+
+        //make sure that the drag value decreased when the apparentWindAngle moves away from 180 degrees
+        return drag;
+    }
+
+    //Calculates the force of lift experienced on the sail, used when broad, beam -reach, and close hauled
+    float CalculateLift(float apparentWindSpeed, float apparentWindAngle)
+    {
+        float lift = .5f * airDensity * apparentWindSpeed * apparentWindSpeed * sailArea * liftCoefficient;
+
+        //make sure that the lift value decreased when the apparentWindAngle moves away from being between around 30 to 135 degrees
+        return lift;
     }
 
     private void RotateRudder()
