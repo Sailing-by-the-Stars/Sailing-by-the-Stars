14d8247429bbb516cfdb2093101d12c3e4824d94
Johan Beimers <35890334+JohanOnPc@users.noreply.github.com>
2026-03-11 16:22:45 +0100

    Updated the lift and drag physics

--- Assets/Boat & Character/Scripts/Boat/BoatController.cs
+++ Assets/Boat & Character/Scripts/Boat/BoatController.cs
@@ -4,15 +4,15 @@
 */
 
 using UnityEngine;
-using UnityEngine.UIElements;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BoatController : MonoBehaviour
 {
     [Header("Physics")]
     [SerializeField] private Vector3 wind = Vector3.zero;
     [SerializeField] private Vector3 apparentWind = Vector3.zero;
-    [SerializeField] private float dragCoefficient = 1.5f;
-    [SerializeField] private float liftCoefficient = 1.2f;
+    [SerializeField] private float dragCoefficient = 1.1f;
+    [SerializeField] private float liftCoefficient = 1.5f;
     [SerializeField] private float sailArea = 20;
     [SerializeField] private float airDensity = 1.225f;
 
@@ -24,9 +24,10 @@ public class BoatController : MonoBehaviour
     [SerializeField] private SmoothAxis2D mainsheetAxis;
 
     [Header("")]
+    [SerializeField] private GameObject hullObject;
     [SerializeField] private GameObject rudderObject;
-    [SerializeField] private GameObject telltaleObject;
     [SerializeField] private GameObject mastObject;
+    [SerializeField] private GameObject sailObject;
 
 
     private Rigidbody rigidBody;
@@ -40,7 +41,7 @@ public class BoatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotateRudder();
+        //RotateRudder();
     }
 
     void FixedUpdate()
@@ -50,31 +51,90 @@ public class BoatController : MonoBehaviour
         Vector3 windDirection = Vector3.ProjectOnPlane(apparentWind, Vector3.up).normalized;
 
         float mastDirectionIntoWind = Vector3.SignedAngle(-mastDirection, windDirection, Vector3.up);
-
-        Debug.Log("Angle of Attack: " + mastDirectionIntoWind);
         //rigidBody.AddRelativeForce(new Vector3(40f ,0f, 0f), ForceMode.Force);
         //rigidBody.AddRelativeTorque(new Vector3(0f, 10f, 0f), ForceMode.Force);
 
-        float drag = CalculateDrag(apparentWind.magnitude, mastDirectionIntoWind);
-        float lift = CalculateLift(apparentWind.magnitude, mastDirectionIntoWind);
+        CalculateDrag(apparentWind.magnitude, mastDirectionIntoWind);
+        CalculateLift(apparentWind.magnitude, mastDirectionIntoWind);
     }
 
     //Calculates the force of drag experienced on the sail, used when running downwind and broadreach
-    float CalculateDrag(float apparentWindSpeed, float apparentWindAngle)
+    void CalculateDrag(float apparentWindSpeed, float apparentWindAngle)
     {
         float drag = .5f * airDensity * apparentWindSpeed * apparentWindSpeed * sailArea * dragCoefficient;
 
+        if (apparentWindAngle > 135f)
+        {
+            drag *= Mathf.SmoothStep(0.1f, 1f, (apparentWindAngle - 135f) / 45f);
+        }
+        else if (apparentWindAngle < -135f)
+        {
+            drag *= Mathf.SmoothStep(0.1f, 1f, (-apparentWindAngle - 135f) / 45f);
+        }
+        else
+            drag *= .1f;
+
+        Debug.Log("Drag: " + drag);
+
         //make sure that the drag value decreased when the apparentWindAngle moves away from 180 degrees
-        return drag;
+        ApplyDrag(drag, apparentWindAngle);
     }
 
     //Calculates the force of lift experienced on the sail, used when broad, beam -reach, and close hauled
-    float CalculateLift(float apparentWindSpeed, float apparentWindAngle)
+    void CalculateLift(float apparentWindSpeed, float apparentWindAngle)
     {
         float lift = .5f * airDensity * apparentWindSpeed * apparentWindSpeed * sailArea * liftCoefficient;
 
+        if (apparentWindAngle < 135f && apparentWindAngle > 0f)
+        {
+            lift *= Mathf.SmoothStep(1f, .1f, apparentWindAngle / 135f);
+        }
+        else if (apparentWindAngle > -135f && apparentWindAngle < 0f)
+        {
+            lift *= Mathf.SmoothStep(1f, .1f, -apparentWindAngle / 135f);
+        }
+        else
+            lift *= .1f;
+
+        Debug.Log("Lift: " + lift);
+
         //make sure that the lift value decreased when the apparentWindAngle moves away from being between around 30 to 135 degrees
-        return lift;
+        ApplyLift(lift, apparentWindAngle);
+    }
+
+    void ApplyLift(float lift, float apparentWindAngle)
+    {
+        //calculate the drag vector perpendicular to the apparent wind direction
+        Vector3 liftVector = Vector3.ProjectOnPlane(apparentWind, Vector3.up).normalized;
+
+        if (apparentWindAngle > 0f)
+            liftVector = Quaternion.AngleAxis(90f, Vector3.up) * liftVector;
+        else
+            liftVector = Quaternion.AngleAxis(-90f, Vector3.up) * liftVector;
+
+        liftVector *= lift;
+
+        liftVector = hullObject.transform.InverseTransformVector(liftVector);
+
+        //retrieve the local y component of the lift vector
+        Vector3 localLiftVector = new Vector3(0f, liftVector.y, 0f);
+        liftVector = hullObject.transform.TransformVector(localLiftVector);
+        
+        rigidBody.AddForce(liftVector, ForceMode.Force);
+    }
+
+    void ApplyDrag(float drag, float apparentWindAngle)
+    {
+        //calculate the drag vector parallel to the apparent wind direction
+        Vector3 dragVector = Vector3.ProjectOnPlane(apparentWind, Vector3.up).normalized * drag;
+
+        dragVector = hullObject.transform.InverseTransformVector(dragVector);
+
+        //retrieve the local y component of the drag vector
+        Vector3 LocalDragVector = new Vector3(0f, dragVector.y, 0f);
+        dragVector = hullObject.transform.TransformVector(LocalDragVector);
+
+        rigidBody.AddForce(dragVector, ForceMode.Force);
     }
 
     private void RotateRudder()
