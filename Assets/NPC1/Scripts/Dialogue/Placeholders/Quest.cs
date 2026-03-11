e7c4c74ba7e6137ec556c6c0012e6a8737a7138a
JGSPanda <115741334+JGSPanda@users.noreply.github.com>
2026-03-11 19:36:22 +0100

    Quest Fix

--- Assets/NPC1/Scripts/Dialogue/Placeholders/Quest.cs
+++ Assets/NPC1/Scripts/Dialogue/Placeholders/Quest.cs
@@ -1,7 +1,7 @@
-using UnityEngine;
-[CreateAssetMenu(fileName = "Quest", menuName = "Placeholder/Quest")]
-// Placeholder!!! Created by Jantina
-public class Quest : ScriptableObject
-{
-    public string questName;
-}
+// using UnityEngine;
+// [CreateAssetMenu(fileName = "Quest", menuName = "Placeholder/Quest")]
+// // Placeholder!!! Created by Jantina
+// public class Quest : ScriptableObject
+// {
+//     public string questName;
+// }
