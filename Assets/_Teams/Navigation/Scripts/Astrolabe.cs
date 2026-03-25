72be932ddb616f0a4802e2efa38fe07c8e476af0
JardiWester <jj.wester102@gmail.com>
2026-03-25 18:28:18 +0100

    added star spheres

--- Assets/_Teams/Navigation/Scripts/Astrolabe.cs
+++ Assets/_Teams/Navigation/Scripts/Astrolabe.cs
@@ -1,5 +1,9 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Astrolabe : MonoBehaviour
 {
@@ -12,10 +16,48 @@ public class Astrolabe : MonoBehaviour
     private AnimationCurve zoomCurve;
     
     private bool zoomedIn;
+    private bool visible = false;
+
+    private List<Renderer> renderers = new();
+    private List<TMP_Text> textBoxes = new();
+
+    private Quaternion initialRot = new();
+
+    private Transform pointer;
+    float pointerAngle;
+
+    private Coroutine coroutine;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+
     void Start()
     {
+        initialRot = transform.localRotation;
+
+        if(pointer == null)
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.name == "Pointer")
+                {
+                    pointer = child;
+                }
+            }
+        }
+
+        renderers = GetComponentsInChildren<Renderer>().ToList();
+        textBoxes = GetComponentsInChildren<TMP_Text>().ToList();
+
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = false;
+        }
+
+        foreach (TMP_Text text in textBoxes)
+        {
+            text.enabled = false;
+        }
+
         cam = GetComponentInParent<Camera>();
         
         initialFOV = cam.fieldOfView;
@@ -28,49 +70,119 @@ public class Astrolabe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        //Vector3 parentEuler = transform.parent.eulerAngles;
+        //transform.rotation = Quaternion.Euler(parentEuler.x, parentEuler.y, 0f);
+
+        //transform.rotation = transform.parent.rotation * initialRot;
+
+
+        Vector3 forward = transform.parent.forward;
+        forward.y = 0f; // remove vertical tilt
+        forward.Normalize();
+
+        transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+
+
+
+        if (Input.GetMouseButtonDown(1) && visible)
         {
             if (zoomedIn)
             {
-                StartCoroutine(ZoomOut());
                 zoomedIn = false;
-                transform.rotation = transform.parent.rotation;
+                if (coroutine == null)
+                {
+                    coroutine = StartCoroutine(ZoomOut());
+                }
             }
             else
             {
-                StartCoroutine(ZoomIn());
                 zoomedIn = true;
+                if (coroutine == null)
+                {
+                    coroutine = StartCoroutine(ZoomIn());
+                }
             }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            visible = !visible;
+            foreach (Renderer rend in renderers)
+            {
+                rend.enabled = visible;
+            }
+            foreach (TMP_Text text in textBoxes)
+            {
+                text.enabled = visible;
+
+                if(visible == false)
+                {
+                    if (zoomedIn)
+                    {
+                        zoomedIn = false;
+                        if (coroutine == null)
+                        {
+                            coroutine = StartCoroutine(ZoomOut());
+                        }
+                    }
+                }
+            }
+        }
+
+
+        pointerAngle = pointer.transform.localRotation.eulerAngles.x;
+        pointerAngle = 360 - pointerAngle;
+
+        if (pointerAngle < 0)
+        {
+            pointerAngle = 0;
+        }
+        if (pointerAngle >= 360)
+        {
+            pointerAngle = 0;
+        }
+
+        foreach (TMP_Text text in textBoxes)
+        {
+            text.text = Mathf.Round(pointerAngle).ToString();
         }
 
         if (zoomedIn)
         {
-            if(Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(new Vector3(0, 0, 10) * Time.deltaTime);
-            }
 
-            if (Input.GetKey(KeyCode.D))
+            float scroll = Input.mouseScrollDelta.y;
+
+            if (scroll != 0f)
             {
-                transform.Rotate(new Vector3(0, 0, -10) * Time.deltaTime);
+                pointer.Rotate(new Vector3(scroll * 10f, 0, 0) * Time.deltaTime);
             }
 
             //Reset astrolabe rotation
             if (Input.GetKey(KeyCode.Space))
             {
-                transform.rotation = transform.parent.rotation;
+                pointer.transform.localRotation = Quaternion.Euler(0, 0, 0);
             }
         }
     }
 
-    private IEnumerator ZoomIn()
+    private IEnumerator ZoomIn(float timer = 0)
     {
-        float timer = zoomTime;
+        if (timer == 0)
+        {
+            timer = zoomTime;
+        }
 
         while (timer > 0)
         {
             timer -= Time.deltaTime;
-            
+
+            if (zoomedIn == false)
+            {
+                coroutine = StartCoroutine(ZoomOut(timer));
+                yield break;
+            }
+
             float T =  timer / zoomTime;
             float curveOutput = zoomCurve.Evaluate(T);
             
@@ -78,15 +190,26 @@ public class Astrolabe : MonoBehaviour
             
             yield return new WaitForEndOfFrame();
         }
+
+        coroutine = null;
     }
     
-    private IEnumerator ZoomOut()
+    private IEnumerator ZoomOut(float timer = 0)
     {
-        float timer = 0;
+        if (timer == 0)
+        {
+            timer = 0;
+        }
 
         while (timer < zoomTime)
         {
             timer += Time.deltaTime;
+
+            if(zoomedIn == true)
+            {
+                coroutine = StartCoroutine(ZoomIn(timer));
+                yield break;
+            }
             
             float T =  timer / zoomTime;
             float curveOutput = zoomCurve.Evaluate(T);
@@ -95,5 +218,7 @@ public class Astrolabe : MonoBehaviour
             
             yield return new WaitForEndOfFrame();
         }
+
+        coroutine = null;
     }
 }
