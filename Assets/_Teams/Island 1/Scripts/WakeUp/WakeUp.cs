7e9b36c4f8236c6ee2aca767ac8abd1d347b1f78
JoelMisiedjan <116649751+JoelMisiedjan@users.noreply.github.com>
2026-03-25 17:17:46 +0100

    Wake Up Effect 0.2
    Changes in WakeUp.cs:
    
    * GameObjects are now private instead of public
    * game objects are now found by using transform.Find("GameObjectName").gameObject, instead of GameObject.Find("GameObjectName")
    * Animation now uses Get Component<Animation>(), instead of deprecated .animation
    
    Change in WakeUpTop:
    Slight alteration in how low it goes in order to get a complete closing effect.

--- Assets/_Teams/Island 1/Scripts/WakeUp/WakeUp.cs
+++ Assets/_Teams/Island 1/Scripts/WakeUp/WakeUp.cs
@@ -3,23 +3,23 @@ using UnityEngine;
 
 public class WakeUp : MonoBehaviour
 {
-    public GameObject topLid; //Declares a variable to hold the reference to the top eyelid game object
-    public GameObject bottomLid; //Declares a variable to hold the reference to the bottom eyelid game object
+    private GameObject topLid; //Declares a variable to hold the reference to the top eyelid game object
+    private GameObject bottomLid; //Declares a variable to hold the reference to the bottom eyelid game object
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       topLid = GameObject.Find("Top_Eyelid"); //Finds the top eyelid game object in the scene and assigns it to the variable
-       bottomLid = GameObject.Find("Bottom_Eyelid"); //Finds the bottom eyelid game object in the scene and assigns it to the variable
+        topLid = transform.Find("Top Eyelid").gameObject; //Finds the child game object named "Top Eyelid" and assigns it to the topLid variable
+        bottomLid = transform.Find("Bottom Eyelid").gameObject; //Finds the child game object named "Bottom Eyelid" and assigns it to the bottomLid variable
     }   
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) //Checks if the left mouse button is pressed down
         {
-            topLid.animation.Play("WakeUpTop"); //Plays the animation on the top eyelid
-            bottomLid.animation.Play("WakeUpBottom"); //Plays the animation on the bottom eyelid
+            topLid.GetComponent<Animation>().Play("WakeUpTop"); //Plays the animation on the top eyelid
+            bottomLid.GetComponent<Animation>().Play("WakeUpBottom"); //Plays the animation on the bottom eyelid
         }
     }
 }
