165e1e92e3db52ef8a56174b8b738629f66ebe9d
Alonso2880 <40578797+Alonso2880@users.noreply.github.com>
2026-04-03 20:01:13 +0200

    Some sounds

---
+++ Assets/_Teams/World Design/Scripts/Sounds/Music/SetAngerEventMusic.cs
@@ -0,0 +1,75 @@
+using FMOD.Studio;
+using FMODUnity;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+/// <summary>
+/// Code by Alonso
+/// </summary>
+public class SetAngerEventMusic : MonoBehaviour
+{
+    [SerializeField] private EventReference fmodEvent;
+
+    [Header("Values")]
+    private float originalValue = 0f;
+    private float duration = 3f;
+    private float currentValue;
+
+    [Header("Reference")]
+    private Coroutine routine;
+    private EventInstance instance;
+    private PARAMETER_ID eventMusicEQ;
+    void Start()
+    {
+        instance = RuntimeManager.CreateInstance(fmodEvent);
+
+        instance.getDescription(out EventDescription desc);
+        desc.getParameterDescriptionByName("EventMusicEQ", out PARAMETER_DESCRIPTION pdesc);
+        eventMusicEQ = pdesc.id;
+
+        currentValue = originalValue;
+
+        instance.start();
+        instance.setParameterByID(eventMusicEQ, currentValue);
+    }
+
+    //Function to set a new value to the anger music
+    public void SetAngerMusic(float target)
+    {
+        if (routine != null) StopCoroutine(routine);
+        routine = StartCoroutine(LerpAngerMusic(target));
+    }
+
+    //Funtion to reset the anger music
+    public void ResetAngerMusic()
+    {
+        SetAngerMusic(originalValue);
+    }
+
+    //Coroutine that changes the anger music value in a progression
+    private IEnumerator LerpAngerMusic(float target)
+    {
+        float start = currentValue;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            currentValue = Mathf.Lerp(start, target, time / duration);
+            instance.setParameterByID(eventMusicEQ, currentValue);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        currentValue = target;
+        instance.setParameterByID(eventMusicEQ, currentValue);
+    }
+
+    private void OnDestroy()
+    {
+        instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        instance.release();
+    }
+}
