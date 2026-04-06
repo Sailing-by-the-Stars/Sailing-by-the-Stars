28d0f18968df3fd10af3d4616c5d0b4fbd63bc40
JGSPanda <115741334+JGSPanda@users.noreply.github.com>
2026-04-06 22:41:04 +0200

    Spin fix

--- Assets/_Teams/NPC 1/Scripts/Dialogue/StateMachine/TempStateMachine.cs
+++ Assets/_Teams/NPC 1/Scripts/Dialogue/StateMachine/TempStateMachine.cs
@@ -1,39 +1,62 @@
 using UnityEngine;
-public enum GameState
-{
-    Moving,
-    Dialogue
-}
+using System.Collections;
+
+public enum GameState { Moving, Dialogue }
+
 public class TempStateMachine : MonoBehaviour
 {
     private Movement movement;
-
     public static TempStateMachine Instance;
     public GameState gameState;
+
+    private GameState previousState;
 
     public void Awake()
     {
         Instance = this;
         gameState = GameState.Moving;
+        previousState = GameState.Moving;
         movement = FindFirstObjectByType<Movement>().GetComponent<Movement>();
     }
 
-    public void FixedUpdate()
+    // Use Update instead of FixedUpdate so cursor lock
+    // is applied before Movement.FixedUpdate runs
+    public void Update()
     {
+        // Detect state change
+        if (gameState != previousState)
+        {
+            if (gameState == GameState.Dialogue)
+                StartCoroutine(DisableMovementNextFrame());
+            else
+            {
+                movement.enabled = true;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            previousState = gameState;
+        }
+
         switch (gameState)
         {
             case GameState.Moving:
-                movement.enabled = true;
                 Cursor.visible = false;
                 Cursor.lockState = CursorLockMode.Locked;
                 break;
             case GameState.Dialogue:
-                movement.enabled = false;
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
                 break;
-            default:
-                break;
         }
+    }
+
+    // Waits one frame so the Input System can flush
+    // its delta before movement is disabled
+    private IEnumerator DisableMovementNextFrame()
+    {
+        yield return null; // wait 1 frame
+        movement.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
