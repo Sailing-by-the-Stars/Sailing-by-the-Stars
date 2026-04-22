b448cb8c27c88a5bf1271ff22379f19844e8d36b
Eduard Ciofu <cfedward7@gmail.com>
2026-04-22 16:38:43 +0200

    morse audio and placeholder lighthouse light

--- Assets/_Teams/Island 2/Scripts/Depression Puzzle/MorseButton.cs
+++ Assets/_Teams/Island 2/Scripts/Depression Puzzle/MorseButton.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 public class MorseButton : MonoBehaviour, IInteractable
 {
     [SerializeField] private MorseDecoder morseDecoder;
-    [SerializeField] private string objectInteractMessage;
+    private MorseAudio morseAudio;
     
+    [SerializeField] private string objectInteractMessage;
     public string InteractMessage => objectInteractMessage;
     public bool ShouldShowMessage => true;
-    
+
+    private void Start()
+    {
+        morseAudio = morseDecoder.GetComponent<MorseAudio>();
+    }
+
     public void HoldInteract(InteractionController interactionController)
     {
         morseDecoder.BeginSignal();
+        morseAudio.StartBeep();
     }
 
     public void ReleaseInteract(InteractionController interactionController)
     {
         morseDecoder.EndSignal();
+        morseAudio.StopBeep();
     }
 }
