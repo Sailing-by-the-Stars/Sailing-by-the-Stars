3b2ea2be65f98fbf224af5264ab88f134296d31e
DigitDogs <digitdogs@gmail.com>
2026-05-11 20:07:27 +0200

    Small fixes to the player inputs handling

--- Assets/_Teams/NPC 1/Scripts/Dialogue/StateMachine/TempStateMachine.cs
+++ Assets/_Teams/NPC 1/Scripts/Dialogue/StateMachine/TempStateMachine.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public enum GameState { Moving, Dialogue, Sailing }
+public enum GameState { Moving, Dialogue, Sailing, Journal, Astrolabe}
 
 public class TempStateMachine : MonoBehaviour
 {
@@ -15,7 +15,7 @@ public class TempStateMachine : MonoBehaviour
 
         PlayerControls = new PlayerControls();
 
-        PlayerControls.Enable();
+        //PlayerControls.Enable();
 
         PlayerControls.Debug.Enable();
 
@@ -33,27 +33,60 @@ public class TempStateMachine : MonoBehaviour
 
         gameState = newState;
 
-        PlayerControls.Land.Disable();
+        /*PlayerControls.Land.Disable();
         PlayerControls.Looking.Disable();
         PlayerControls.BoatSail.Disable();
         PlayerControls.BoatRudder.Disable();
         PlayerControls.BoatAnchor.Disable();
-        PlayerControls.Dialogue.Disable();
+        PlayerControls.Dialogue.Disable();*/
+
+        PlayerControls.Disable();
+        PlayerControls.Debug.Enable();
 
         switch (newState)
         {
             case GameState.Moving:
                 PlayerControls.Land.Enable();
-                PlayerControls.Looking.Enable();
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
+                PlayerControls.Looking.Enable(); 
+                SetCursorVisibility(false);
                 break;
 
             case GameState.Dialogue:
-                PlayerControls.Dialogue.Enable();
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
+                PlayerControls.Dialogue.Enable(); 
+                SetCursorVisibility(true);
                 break;
+
+            case GameState.Sailing:
+                PlayerControls.Land.Enable();
+                PlayerControls.Looking.Enable();
+                SetCursorVisibility(false);
+                break;
+
+            case GameState.Journal:
+
+                break;
+
+            case GameState.Astrolabe:
+
+                break;
+
+            default:
+                Debug.LogError("The player state is set completely wrong.");
+                break;
+        }
+    }
+
+    void SetCursorVisibility(bool state)
+    {
+        if (state)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
 }
