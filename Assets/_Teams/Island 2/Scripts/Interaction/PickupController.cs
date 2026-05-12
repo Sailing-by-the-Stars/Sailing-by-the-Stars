c0b88ec82f95472f5d21d3d3291d604b55a149ec
DigitDogs <digitdogs@gmail.com>
2026-05-12 15:35:54 +0200

    Interaction system updated to input system.

--- Assets/_Teams/Island 2/Scripts/Interaction/PickupController.cs
+++ Assets/_Teams/Island 2/Scripts/Interaction/PickupController.cs
@@ -54,8 +54,7 @@ public class PickupController : MonoBehaviour
 
     private void CheckDropInput()
     {
-        // TODO: replace hardcoded key press with Input Actions
-        if (!Keyboard.current.qKey.wasPressedThisFrame || !HasPickup) return;
+        if (!playerControls.Interaction.Drop.WasPerformedThisFrame() || !HasPickup) return;
         
         currentPickup.Drop(this);
         currentPickup = null;
@@ -63,8 +62,7 @@ public class PickupController : MonoBehaviour
 
     private void CheckPickupInput()
     {
-        // TODO: replace hardcoded key press with Input Actions
-        if (!Mouse.current.leftButton.wasPressedThisFrame || !HasPickup) return;
+        if (!playerControls.Interaction.Use.WasPerformedThisFrame() || !HasPickup) return;
 
         currentPickup.Use();
     }
